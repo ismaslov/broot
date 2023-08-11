b5d34b2b5a862e02322bf1c0fb52fc99b2315779
Igor Maslov <i.maslov@krista.ru>
2023-08-11 16:48:49 +0300

    some mapping rules for icons

--- resources/icons/vscode/data/file_name_to_icon_name_map.rs
+++ resources/icons/vscode/data/file_name_to_icon_name_map.rs
@@ -2,4 +2,7 @@
 	( "license"     , "file_type_license"           ),
 	( "todo"        , "file_type_todo"              ),
 	( "readme"      , "file_type_text"              ),
+	( "makefile"    , "file_type_makefile"          ),
+	( "go.mod"      , "file_type_go_package"        ),
+	( "go.sum"      , "file_type_go_package"        ),
 ]
